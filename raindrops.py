ce17d4c0364ad26dcce9cdb724b57151184f9d4c
p4v31 <90404785+p4v31@users.noreply.github.com>
2021-09-12 17:07:20 +0300

    Update raindrops.py

--- raindrops.py
+++ raindrops.py
@@ -1,14 +1,12 @@
 def convert(number):
-    t=False
-    if number%3==0 and t==False:
-        return('Pling')
-        t=True
-    elif number%5==0 and t==False:
-        return('Plang')
-        t = True
-    elif number%7==0 and t==False:
-        return('Plong')
-        t=True
-    elif number%3!=0 and number%5!=0 and number%7!=0 and t==False:
-        return(int(number))
-        t=True
+    a=[]
+    if number%3==0:
+        a.append('Pling')
+    if number%5==0:
+        a.append('Plang')
+    if number%7==0:
+        a.append('Plong')
+    if number%3!=0 and number%5!=0 and number%7!=0:
+        a.append(str(number))
+    b=str(''.join(a))
+    return(b)
